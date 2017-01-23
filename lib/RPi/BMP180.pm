54b68edee1481b48f8727461652989148d089358
Steve Bertrand <steve02@hitachi-id.com>
2017-01-23 09:26:10 -0700

    merged module from RPi::WiringPi

--- lib/RPi/BMP180.pm
+++ lib/RPi/BMP180.pm
@@ -3,108 +3,111 @@ package RPi::BMP180;
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+our $VERSION = '2.36.1';
 
-require XSLoader;
-XSLoader::load('RPi::BMP180', $VERSION);
+use WiringPi::API qw(:all);
+use RPi::WiringPi::Constant qw(:all);
+
+sub new {
+    my ($class, $pin_base) = @_;
+    my $self = bless {}, $class;
+    $self->_pin_base($pin_base);
+   
+    setup_gpio(); 
+    bmp180_setup($pin_base);
+
+    return $self;
+}
+sub temp {
+    my ($self, $want) = @_;
+    return bmp180_temp($self->_pin_base + 0, $want);
+}
+sub pressure {
+    my ($self) = @_;
+    return bmp180_pressure($self->_pin_base + 1);
+}
+sub _pin_base {
+    my ($self, $base) = @_;
+
+    if (defined $base){
+        if ($base !~ /^\d+$/){
+            die "_pin_base() requires an integer\n";
+        }
+        $self->{bmp_pin_base} = $base;
+    }
+
+    if (! defined $self->{bmp_pin_base}){
+        die "_pin_base() has not yet been set...\n";
+    }
+    return $self->{bmp_pin_base};
+}
+sub _vim{};
 
 1;
 __END__
 
 =head1 NAME
 
-RPi::BMP180 - Interface to the BMP180 barometric/altimeter sensor on Raspberry
-Pi
+RPi::BMP180 - Interface to the BMP180 barometric pressure sensor
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
     use RPi::BMP180;
 
-    my $foo = RPi::BMP180->new();
-    ...
+    my $base = 300;
 
-=head1 EXPORT
+    my $bmp = RPi::BMP180($base);
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+    my $f = $bmp->temp;
+    my $c = $bmp->temp('c');
+    my $p = $bmp->pressure; # kPa
 
-=head1 SUBROUTINES/METHODS
+=head1 DESCRIPTION
 
-=head2 function1
+This module allows you to interface with a BMP180 barometric and temperature
+sensor. 
 
-=cut
+=head1 METHODS
 
-sub function1 {
-}
+=head2 new($pin_base)
 
-=head2 function2
+Returns a new C<RPi::BMP180> object.
 
-=cut
+Parameters:
 
-sub function2 {
-}
+    $pin_base
+
+Mandatory: Integer, the number at which to start the 'pseudo' GPIO pins for
+communication to the sensor. Anything above the highest numbered GPIO pin will
+do. For example, C<100> or C<200>.
+
+=head2 temp
+
+Fetches the temperature from the sensor.
+
+Parameters:
+
+    $want
+
+Optional: String. By default, we return Farenheit. To get Celcius, pass in
+C<'c'>.
+
+Returns a floating point number.
+
+=head2 pressure
+
+Fetches the barometric pressure in kPa.
+
+Takes no parameters, returns a floating point number.
 
 =head1 AUTHOR
 
-Steve Bertrand, C<< <steveb at cpan.org> >>
+Steve Bertrand, E<lt>steveb@cpan.orgE<gt>
 
-=head1 BUGS
+=head1 COPYRIGHT AND LICENSE
 
-Please report any bugs or feature requests to C<bug-rpi-bmp180 at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=RPi-BMP180>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+Copyright (C) 2016 by Steve Bertrand
 
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc RPi::BMP180
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=RPi-BMP180>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/RPi-BMP180>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/RPi-BMP180>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/RPi-BMP180/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2017 Steve Bertrand.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See L<http://dev.perl.org/licenses/> for more information.
-
-
-=cut
-
-1; # End of RPi::BMP180
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.18.2 or,
+at your option, any later version of Perl 5 you may have available.
